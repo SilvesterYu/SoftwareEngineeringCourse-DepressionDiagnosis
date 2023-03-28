b7bf943b1185c5161922f0a36ff6c731bd5a2bd0
Silvester Yu <ly1164@nyu.edu>
2023-03-28 17:10:14 +0800

    testing with redirect

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.post('/upload', (req, res) => {
         let theFileString = req.body.theFile;
         let theFileName = req.body.name;
         theFileString = theFileString.replace(/data:image\/(png|jpeg);base64,/, "");
-        console.log('the file string ' + theFileString);
+        // console.log('the file string ' + theFileString);
         fs.writeFileSync('images/' + theFileName, theFileString, {
             'encoding': 'base64'
         });
