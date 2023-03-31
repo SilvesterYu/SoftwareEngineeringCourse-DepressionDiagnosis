75a5c2d1107e8b88cfa2b911d008ccda955e413e
Momoe <mn2668@nyu.edu>
2023-03-31 18:48:32 +0800

    add database file with schemas :

---
+++ db.js
@@ -0,0 +1,83 @@
+const fs = require("fs");
+const path = require("path");
+const url = require("url");
+const mongoose = require("mongoose");
+const mongooseSlugPlugin = require("mongoose-slug-plugin");
+
+//const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+
+const AccountSchema = new mongoose.Schema(
+  {
+    username: { type: String, unique: true, required: true },
+    password: { type: String, unique: true, required: true },
+    email: { type: String, unique: true, required: true },
+    role: {
+      type: String,
+      required: true,
+      default: "user",
+      enum: ["user", "admin"],
+    },
+    reports: [{ type: mongoose.Schema.Types.ObjectId, ref: "Report" }],
+    posts: [{ type: mongoose.Schema.Types.ObjectId, ref: "Post" }],
+  },
+  { timestamps: true }
+);
+
+const ReportSchema = new mongoose.Schema(
+  {
+    account: { type: mongoose.Schema.Types.ObjectId, ref: "Account" },
+    score: { type: Number },
+    description: { type: String },
+    starred: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
+
+const PostSchema = new mongoose.Schema(
+  {
+    account: { type: mongoose.Schema.Types.ObjectId, ref: "Account" },
+    title: { type: String, required: true },
+    content: { type: String },
+    type: {
+      type: String,
+      default: "regular",
+      enum: ["regular", "announcement"],
+    },
+    replies: [{ type: mongoose.Schema.Types.ObjectId, ref: "Reply" }],
+  },
+  { timestamps: true }
+);
+
+const ReplySchema = new mongoose.Schema(
+  {
+    account: { type: mongoose.Schema.Types.ObjectId, ref: "Account" },
+    post: { type: mongoose.Schema.Types.ObjectId, ref: "Post" },
+    content: { type: String, required: true },
+  },
+  { timstamps: true }
+);
+
+AccountSchema.plugin(mongooseSlugPlugin, { tmpl: "<%=username%>" });
+
+mongoose.model("Account", AccountSchema);
+mongoose.model("Report", ReportSchema);
+mongoose.model("Post", PostSchema);
+mongoose.model("Reply", ReplySchema);
+
+let dbconf;
+if (process.env.NODE_ENV === "PRODUCTION") {
+  // if we're in PRODUCTION mode, then read the configration from a file
+  // use blocking file io to do this...
+  const fn = path.join(__dirname, "config.json");
+  const data = fs.readFileSync(fn);
+
+  // our configuration file will be in json, so parse it and set the
+  // conenction string appropriately!
+  const conf = JSON.parse(data);
+  dbconf = conf.dbconf;
+} else {
+  // if we're not in PRODUCTION mode, then use
+  dbconf = "mongodb://localhost/depression-diagnosis-js";
+}
+
+mongoose.connect(dbconf);
