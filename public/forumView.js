78bada25b04798b84635802223e01af91c0450d0
Silvester Yu <ly1164@nyu.edu>
2023-04-02 13:32:41 +0800

    forum view now displays author account

--- public/forumView.js
+++ public/forumView.js
@@ -12,12 +12,16 @@ function getPosts() {
       posts.forEach((post) => {
         console.log(post);
         let title = document.createElement("div");
+        let author = document.createElement("div");
         let body = document.createElement("div");
         title.innerHTML = post.title;
+        author.innerHTML = post.account;
         body.innerHTML = post.content;
         title.className = "forumViewTitle";
+        author.className = "forumViewAuthor";
         body.className = "forumViewBody";
-        title.appendChild(body);
+        title.appendChild(author);
+        author.appendChild(body);
         discussionPosts.appendChild(title);
       });
     });
